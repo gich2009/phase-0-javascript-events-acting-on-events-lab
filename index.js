bf150ccc50d404c40dde59a6d923aba29ce2fbc9
gich2009 <waneneamos@gmail.com>
2023-03-25 23:13:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+function moveDodgerLeft(){
+  const leftCoordinateString = dodger.style.left.replace("px", "");
+  const leftCoordinate = parseInt(leftCoordinateString, 10);
+
+  if (leftCoordinate > 0){
+    dodger.style.left = `${leftCoordinate - 1}px`;
+  }
+}
+
+moveDodgerLeft();
+moveDodgerRight();
+
+function moveDodgerRight(){
+  const leftCoordinateString = dodger.style.left.replace("px", "");
+  const leftCoordinate = parseInt(leftCoordinateString, 10);
+
+  if (leftCoordinate < 360){
+    dodger.style.left = `${leftCoordinate + 1}px`;
+  } 
+
+}
